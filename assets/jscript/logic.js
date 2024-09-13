6d45f673d79cd15d44f0d1125a02100f28d02cad
Ryan Merlini <rdmerlini@gmail.com>
2024-09-13 12:31:18 -0400

    reading local storage of array objects

---
+++ assets/jscript/logic.js
@@ -0,0 +1,21 @@
+
+// // This function renders blog posts to the page
+// function renderPosts() {
+//     // Clear existing content in the container
+//     blogPostsContainer.innerHTML = '';
+
+// Initialize workouts from localStorage
+console.log(JSON.parse(localStorage.getItem('workouts')));
+
+// function init() {
+//     const storedWorkouts = JSON.parse(localStorage.getItem('workouts'));
+//     if (storedWorkouts !== null) {
+//       workouts = storedWorkouts;
+//     }
+//     // renderWorkouts();
+//   }
+
+// console.log(stor)
+
+// // INITIALIZATION
+// init();
